2a109c76898b6c48b94aafba47134245d666f3dc
rpaul23 <jott.ess@gmx.net>
2018-01-19 12:40:18 +0100

    Add wholebrain plotting

--- lib/monkeyplot.py
+++ lib/monkeyplot.py
@@ -7,8 +7,13 @@ Created on Thu Jan 18 16:43:55 2018
 """
 
 import matplotlib.pyplot as plt
+import pandas as pd
 
-from . import frommatlab as fmat
+if __name__ == "__main__":
+    import frommatlab as fmat
+else:
+    from . import frommatlab as fmat
+
 
 def plotareas(list_of_areas, coords_path, rinfo_path, rotate=True):
     """Plots electrodes for a given set of target areas onto a map. """
@@ -19,6 +24,34 @@ def plotareas(list_of_areas, coords_path, rinfo_path, rotate=True):
     # Clock-wise rotate by 90 degrees (x, y) --> (y, -x)
     if rotate == True:
         coords[:,0], coords[:,1] = coords[:,1], -coords[:,0].copy()
-    
-    # Plot, return coords if indicated
+        
+    # Plot
     plt.scatter(coords[:,0], coords[:,1])
+
+
+def plotwholebrain(coords_path, rinfo_path, rotate=True):
+    """Plots all  electrodes onto map, colors by region. """
+    # Load coords
+    area_names = fmat.areanames(rinfo_path, unique=False)
+    unique_areas = fmat.areanames(rinfo_path, unique=True, as_list=True)
+    idx = fmat.targetchannels(unique_areas, rinfo_path)
+    coords = fmat.loadcoords(coords_path)[idx]
+    
+    # Clock-wise rotate by 90 degrees (x, y) --> (y, -x)
+    if rotate == True:
+        coords[:,0], coords[:,1] = coords[:,1], -coords[:,0].copy()
+    
+    # Use pandas for grouping
+    df = pd.DataFrame(dict(x=coords[:,0], y=coords[:,1], label=area_names))
+    df_grouped = df.groupby('label')
+    
+    # Plot
+    fig, ax = plt.subplots()
+    ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+    for name, group in df_grouped:
+        ax.plot(group.x, group.y, marker='o', linestyle='', label=name)
+    ax.legend()
+    plt.show()
+    
+plotwholebrain(coords_path, rinfo_path, rotate=True)
+        
