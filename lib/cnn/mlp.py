805963024c7f90a601153771ef4766b378d0efc0
Jannes Schäfer <jott.ess@gmx.net>
2018-04-11 15:28:15 +0200

    Make MLP tunable

--- lib/cnn/mlp.py
+++ lib/cnn/mlp.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
@@ -5,13 +7,13 @@ from sklearn.model_selection import train_test_split
 import myio as io
 import helpers as hlp
 
-def train_classif(x_train, y_train, layer_sizes, activation='relu'):
+def train_classif(x_train, y_train, layer_sizes, alpha, batch_size):
     """Trains a multi-layer perceptron."""
     clf = MLPClassifier(hidden_layer_sizes=layer_sizes,
-                        activation=activation,
+                        activation='relu',
                         solver='adam',
-                        alpha=10,
-                        batch_size=50,
+                        alpha=alpha,
+                        batch_size=batch_size,
                         learning_rate_init=1e-5,
                         max_iter=200)
     clf.fit(x_train, y_train)
@@ -21,6 +23,7 @@ def evaluate_classif(clf, x_test, y_test):
     """Evaluates MLP classifier against test data."""
     return np.mean(clf.predict(x_test) == y_test)
 
+counter = sys.argv[1‚]
 base_path = '/home/jannesschaefer/'
 data_path = base_path + 'data/pre-processed/sample_500/'
 raw_path = base_path + 'data/raw/141023/session01/'
@@ -42,6 +45,21 @@ targets = io.get_targets(decode_for, raw_path, elec_type, n_chans,
                          only_correct_trials=only_correct_trials,
                          onehot=False)
 
+params = {
+    'batch_size': [50, 100],
+    'alpha': [.01, .1, 1, 10, 50],
+    'layer_sizes': [(10,), (20,), (50,), (20, 10), (50, 20)]
+    }
+
+ind_batch_size = 0 if counter < len(params['alpha']) * len(params['layer_sizes']) else 1
+temp = counter if ind_batch_size == 0 else counter - 25
+ind_alpha = int(np.floor(temp/len(params['layer_sizes'])))
+ind_layer_sizes = counter % len(params['layer_sizes'])
+
+batch_size = params['batch_size'][ind_batch_size]
+alpha = params['alpha'][ind_alpha]
+layer_sizes = params['layer_sizes'][ind_layer_sizes]
+
 # train/test params
 samples_per_trial = data.shape[2]
 n_chans = data.shape[1]
@@ -49,7 +67,6 @@ train_size = .8
 test_size = .2
 seed = np.random.randint(1,10000)
 indices = np.arange(data.shape[0])
-layer_sizes = (50)
 
 for i in range(10):
     train, test, train_labels, test_labels, idx_train, idx_test = (
@@ -66,6 +83,9 @@ for i in range(10):
     x_test = test.reshape(test.shape[0], -1)
     y_test = test_labels.reshape(-1)
 
-    clf = train_classif(x_train, y_train, layer_sizes=layer_sizes)
+    clf = train_classif(x_train, y_train,
+                        layer_sizes=layer_sizes,
+                        alpha=alpha,
+                        batch_size=batch_size)
     acc = evaluate_classif(clf, x_test, y_test)
     print(i, acc)
